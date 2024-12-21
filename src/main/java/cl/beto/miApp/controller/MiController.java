8bdf5f1d2c26ead9c921106e26018fe337bf62ce
RobertoVillalon <robertoantonio3756@gmail.com>
2024-12-21 17:09:01 -0300

    Primer commit

---
+++ src/main/java/cl/beto/miApp/controller/MiController.java
@@ -0,0 +1,17 @@
+package cl.beto.miApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MiController {
+
+    @GetMapping
+    public String testFunction(){
+
+        return "Esta es una funcion de prueba";
+    }
+
+}
---
+++ src/test/java/cl/beto/miApp/MiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.beto.miApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
